925eff600e3b79573c2ad986216387e347eda091
Lindsey <lindsey@blacktopinteractive.com>
2021-01-03 20:44:47 -0600

    Game Screen added

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Header from './components/Header/Header';
 import StartGameScreen from './screens/StartGameScreen/StartGameScreen';
+import GameScreen from './screens/GameScreen/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber}/>
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- screens/StartGameScreen/StartGameScreen.js
+++ screens/StartGameScreen/StartGameScreen.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import Card from '../../components/Card/Card';
 import Colors from '../../constants/colors';
 import Input from '../../components/Input/Input';
+import NumberContainer from '../../components/NumberContainer/NumberContainer';
 
 
 const StartGameScreen = props => {
@@ -21,18 +22,26 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+        if (isNaN(chosenNumber)|| chosenNumber <= 0 || chosenNumber > 99) {
+            console.log('here!!')
+            Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99.', [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}])
             return;
         }
         setConfirmedState(true);
         setSelectedNumber(chosenNumber);
         setEnteredValue('');
+        Keyboard.dismiss();
     }
 
     let confirmedOutput;
 
     if (confirmed) {
-        confirmedOutput = <Text> Chosen number is: {selectedNumber}</Text>
+        confirmedOutput =
+        <Card style={styles.summaryContainer}>
+            <Text> You selected </Text>
+            <NumberContainer> {selectedNumber}</NumberContainer>
+            <Button title={"START GAME"} onPress={() => props.onStartGame(selectedNumber)} />
+        </Card>
     }
 
     return(
@@ -97,6 +106,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center',
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center',
     },
 });
 
